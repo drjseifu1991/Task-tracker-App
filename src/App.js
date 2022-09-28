2e962c82b95e4c1abd63e7c8cedf9291b2b038f7
Dereje3030 <51210918+Dereje3030@users.noreply.github.com>
2022-09-28 08:55:51 +0300

    My Second day

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
 import Header from "./components/Header";
+import { useState } from "react";
+import Tasks from "./components/Tasks";
 function App() {
+  const [tasks] = useState([
+    {
+      id : 1,
+      text : 'Doctors Appointement',
+      day : 'Feb 5th at 2:30pm',
+      reminder : true,
+    },
+    {
+      id : 2,
+      text : 'Meeting at school',
+      day : 'Feb 6th at 8:00am',
+      reminder : true
+    },
+    {
+      id : 3,
+      text : 'Food shopping',
+      day : 'Feb 7th at 9:00am',
+      reminder : true
+    },
+    {
+      id : 4,
+      text : 'Play Football',
+      day : 'Feb 8th at 5:00pm',
+      reminder : true
+    }
+  ]);
   return (
-    <div className="App">
+    <div className="container">
       <Header/>
+      <Tasks tasks= {tasks} />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Button from './Button'
 
-const Header = ({title,name}) => {
+const Header = ({title}) => {
+  const onClick = () => {
+    console.log('click');
+  }
   return (
-    <header>
-        {name}
+    <header className='header'>
+        <h1>{title}</h1>
+        <Button color='green' text='Add' onClick={onClick}/>
     </header>
   )
 }
 Header.defaultProps = {
-  name : 'App'
+  title : 'Task-Tracker'
 }
 Header.propTypes = {
     title : PropTypes.string
